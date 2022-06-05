12ea0842003e69e6b0f57ec08ee07f1606b877ea
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-06-05 16:23:21 +0900

    get real time and date

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const footer = () => {
   return (
-    <div className="mt-5 bg-dark text-white h-25 text-center">
+    <div className="mt-5 bg-dark text-white h-25 d-flex justify-content-center align-items-center">
       <p>This is footer</p>
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,57 +1,63 @@
-import React, { useEffect, useState } from 'react'
-import { db } from './firebase'
-import { set, ref, onValue, update } from "firebase/database"
-import { uid } from "uid"
-import { Container } from './styled'
-import Footer from './Footer';
-
+import React, { useEffect, useState } from "react";
+import { db } from "./firebase";
+import { set, ref, onValue, update } from "firebase/database";
+import { uid } from "uid";
+import { Container } from "./styled";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { format } from 'date-fns';
+import Footer from "./Footer";
 
 const Home = () => {
-    const [temp, setTemp] = useState([])
-    const [toggle, setToggle] = useState(false)
-    //FETCHING DATA:
-    useEffect(() => {
-        onValue(ref(db), snapshot => {
-            const data = snapshot.val();
-            if (data !== null) {
-                Object.values(data).map(value => {
-                    setTemp(prevState => [...prevState, value])
-                })
-            }
-            console.log(data);
+  const [temp, setTemp] = useState([]);
+  const [toggle, setToggle] = useState(false);
 
-        })
-    }, [])
+  const currentDate = new Date().toLocaleDateString('en-US');
+  const currentTime = new Date().toLocaleTimeString("en-US");
 
+  //FETCHING DATA:
+  useEffect(() => {
+    onValue(ref(db), (snapshot) => {
+      const data = snapshot.val();
+      if (data !== null) {
+        Object.values(data).map((value) => {
+          setTemp((prevState) => [...prevState, value]);
+        });
+      }
+      console.log(data);
+    });
+  }, []);
 
-    const handleToggle = () => {
-        setToggle(!toggle)
-    }
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
 
-    const handleChange = () => {
-        update(ref(db, "Sensors"), { buzzer: toggle ? "10" : 0 })
-    }
+  const handleChange = () => {
+    update(ref(db, "Sensors"), { buzzer: toggle ? "10" : 0 });
+  };
 
-    useEffect(() => {
-        handleChange();
-    }, [toggle])
+  useEffect(() => {
+    handleChange();
+  }, [toggle]);
 
-    const date = new Date().getMinutes()
-    console.log(date)
+  const date = new Date().getMinutes();
+  console.log(date);
 
-    console.log(temp)
+  console.log(temp);
 
-    return (
-        <Container>
-            <div>
-                <div className='time'>
-                    <p>Buzzer</p>
-                    <button onClick={handleToggle}>On/Off</button>
-                </div>
-            </div>
-            <Footer></Footer>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <div>
+        <div className="time">
+            <h1 className="text-center fw-bold">{currentDate}</h1>
+            <h2 className="text-center">{currentTime}</h2>
+          <p>Buzzer</p>
+          <button onClick={handleToggle}>On/Off</button>
+        </div>
+      </div>
+      <Footer></Footer>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
