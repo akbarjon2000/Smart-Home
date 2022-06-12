5292225bab615b86b75d915c372df07a82ec655c
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-06-12 23:56:19 +0900

    login section also done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,35 +1,52 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import { useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth";
+import {
+  useSendPasswordResetEmail,
+  useSignInWithEmailAndPassword,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import auth from "./firebase";
+import Loading from "./Loading";
 
 const Login = () => {
-    const navigate = useNavigate();
-    const [agree, setAgree] = useState(false);
+  const navigate = useNavigate();
 
-    const navigateRegister = event => {
-        navigate("/signup")
-    }
-    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
-    const [signInWithEmailAndPassword, user, loading, error] =
-      useSignInWithEmailAndPassword(auth);
-  
-    const onSubmit = (event) => {
-      const email = event.target.email.value;
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+  const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+  let errorElement;
+  if (error) {
+    errorElement = <p className="text-danger">Error: {error?.message}</p>
+}
+
+  const navigateRegister = event => {
+      navigate("/signup")
+  }
+  if(loading || sending) {
+    return <Loading></Loading>
+  }
+  if(user) {
+    navigate('/');
+  }
+
+  const onSubmit = (event) => {
+    const email = event.target.email.value;
     const password = event.target.password.value;
-      signInWithEmailAndPassword(email, password);
-    };
-  
-    let signInError;
-      const location = useLocation();
-      let from = location.state?.from?.pathname || "/";
+    signInWithEmailAndPassword(email, password);
+    // console.log(data);
+    navigate('/');
+    event.preventDefault();
+  };
   return (
     <>
       <div className="register-form">
         <h2 className="text-center mt-5">Please Log in</h2>
         <form onSubmit={onSubmit}>
-          
           <input
             type="email"
             name="email"
@@ -46,22 +63,16 @@ const Login = () => {
           />
 
           <div className="form-check">
-            {/* <label className={agree? "ps-2 text-primary" : "ps-2 text-danger"} htmlFor="terms">Accept Genius Car Terms and Conditions</label> */}
-            <label
-              className={`2px ${agree ? "text" : "text-danger"}`}
-              htmlFor="terms"
-            >
-              Haven't account in Smart Home 
-            </label>
+            
           </div>
           <input
             className="bg-primary rounded w-50 mx-auto border-0 text-white mt-2"
             type="submit"
-            value="Log in"
+            value="Login"
           />
         </form>
         <p>
-          Signup?
+          Haven't an account on smart home?
           <Link
             to={"/signup"}
             onClick={navigateRegister}
