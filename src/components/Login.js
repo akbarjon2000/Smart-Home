c388fc1bb31892d124d66a6ec706644ead67d225
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-06-12 23:45:10 +0900

    sign up is done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import auth from "./firebase";
 
 const Login = () => {
     const navigate = useNavigate();
+    const [agree, setAgree] = useState(false);
 
     const navigateRegister = event => {
         navigate("/signup")
     }
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+    const [signInWithEmailAndPassword, user, loading, error] =
+      useSignInWithEmailAndPassword(auth);
+  
+    const onSubmit = (event) => {
+      const email = event.target.email.value;
+    const password = event.target.password.value;
+      signInWithEmailAndPassword(email, password);
+    };
+  
+    let signInError;
+      const location = useLocation();
+      let from = location.state?.from?.pathname || "/";
   return (
     <>
-      <Form className="container w-50 mt-5">
+      <div className="register-form">
+        <h2 className="text-center mt-5">Please Log in</h2>
+        <form onSubmit={onSubmit}>
+          
+          <input
+            type="email"
+            name="email"
+            id=""
+            placeholder="Email Address"
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            id=""
+            placeholder="Your Password"
+            required
+          />
 
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <p>New to Smart Home Application?<Link to={"/signup"} onClick={navigateRegister} className="text-primary pe-auto text-decoration-none">Please Sign up</Link></p>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+          <div className="form-check">
+            {/* <label className={agree? "ps-2 text-primary" : "ps-2 text-danger"} htmlFor="terms">Accept Genius Car Terms and Conditions</label> */}
+            <label
+              className={`2px ${agree ? "text" : "text-danger"}`}
+              htmlFor="terms"
+            >
+              Haven't account in Smart Home 
+            </label>
+          </div>
+          <input
+            className="bg-primary rounded w-50 mx-auto border-0 text-white mt-2"
+            type="submit"
+            value="Log in"
+          />
+        </form>
+        <p>
+          Signup?
+          <Link
+            to={"/signup"}
+            onClick={navigateRegister}
+            className="text-primary pe-auto text-decoration-none"
+          >
+            Please Sign up
+          </Link>
+        </p>
+      </div>
+      );
     </>
   );
 };
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { useRef, useState } from 'react';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useRef, useState } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import "./SignUp.css";
-import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
+import {
+  useCreateUserWithEmailAndPassword,
+  useUpdateProfile,
+} from "react-firebase-hooks/auth";
 import { Button, Form } from "react-bootstrap";
 import auth from "./firebase";
 import Loading from "./Loading";
@@ -10,53 +13,98 @@ import Loading from "./Loading";
 // import auth from "./firebase.init";
 
 const SignUp = () => {
-    const [agree, setAgree] = useState(false);
-    const [ createUserWithEmailAndPassword, user, loading, error ] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification: true});
-    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
-    const navigate = useNavigate();
-    const navigateLogin = event => {
-        navigate("/login");
-    }
-    if(loading || updating) {
-        return <Loading></Loading>
-    }
-    
-    const handleRegister = async (event) => {
-        event.preventDefault();
-        const name = event.target.name.value;
-        const email = event.target.email.value;
-        const password = event.target.password.value;
-        // const agree = event.target.terms.checked;
+  const [agree, setAgree] = useState(false);
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+  const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+  const navigate = useNavigate();
+  const navigateLogin = (event) => {
+    navigate("/login");
+  };
+  if (loading || updating) {
+    return <Loading></Loading>;
+  }
 
-        await createUserWithEmailAndPassword(email, password);
-        await updateProfile({ displayName: name });
-        console.log('Updated profile');
-        navigate('/');
-    }
-    if (user) {
-        console.log('user', user);  
-    }
+  const handleRegister = async (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    // const agree = event.target.terms.checked;
+
+    await createUserWithEmailAndPassword(email, password);
+    await updateProfile({ displayName: name });
+    console.log("Updated profile");
+    navigate("/");
+  };
+  if (user) {
+    console.log("user", user);
+  }
   return (
     <>
-              <div className='register-form'>
-            <h2 className='text-center mt-5'>Please Sign up</h2>
-            <form onSubmit={handleRegister}>
-                <input  type="text" name="name" id="" placeholder='Your Name' required/>
-                <input  type="email" name="email" id="" placeholder='Email Address' required/>
-                <input  type="password" name="password" id="" placeholder='Your Password' required/>
+      <div className="register-form">
+        <h2 className="text-center mt-5">Please Sign up</h2>
+        <form onSubmit={handleRegister}>
+          <input
+            type="text"
+            name="name"
+            id=""
+            placeholder="Your Name"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            id=""
+            placeholder="Email Address"
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            id=""
+            placeholder="Your Password"
+            required
+          />
 
-                <div className="form-check">
-                <input onClick={() => setAgree(!agree)} className="form-check-input" type="checkbox" name="terms" value="" id='terms'/>
-                {/* <label className={agree? "ps-2 text-primary" : "ps-2 text-danger"} htmlFor="terms">Accept Genius Car Terms and Conditions</label> */}
-                <label className={`2px ${agree ? "text" : "text-danger"}`} htmlFor="terms">Accept Smart Home Terms and Conditions</label>
-                </div>
-                <input disabled={!agree} className='bg-primary rounded w-50 mx-auto border-0 text-white mt-2' type="submit" value="Register" />
-            </form>
-            <p>Already have an account?<Link to={"/login"} onClick={navigateLogin} className="text-primary pe-auto text-decoration-none">Please Sign up</Link></p>
-        </div>
-    );
+          <div className="form-check">
+            <input
+              onClick={() => setAgree(!agree)}
+              className="form-check-input"
+              type="checkbox"
+              name="terms"
+              value=""
+              id="terms"
+            />
+            {/* <label className={agree? "ps-2 text-primary" : "ps-2 text-danger"} htmlFor="terms">Accept Genius Car Terms and Conditions</label> */}
+            <label
+              className={`2px ${agree ? "text" : "text-danger"}`}
+              htmlFor="terms"
+            >
+              Accept Smart Home Terms and Conditions
+            </label>
+          </div>
+          <input
+            disabled={!agree}
+            className="bg-primary rounded w-50 mx-auto border-0 text-white mt-2"
+            type="submit"
+            value="Sign Up"
+          />
+        </form>
+        <p>
+          Already have an account?
+          <Link
+            to={"/login"}
+            onClick={navigateLogin}
+            className="text-primary pe-auto text-decoration-none"
+          >
+            Please Login
+          </Link>
+        </p>
+      </div>
+      );
     </>
-    );
+  );
 };
 
 export default SignUp;
